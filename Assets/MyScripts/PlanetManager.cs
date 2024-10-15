9a54a0f85513f7b0bbecf8f7c6fd3611b4ab273d
m23loure <162428618+m23loure@users.noreply.github.com>
2024-10-15 15:33:33 +0200

    gestion des evenements

--- Assets/MyScripts/PlanetManager.cs
+++ Assets/MyScripts/PlanetManager.cs
@@ -1,10 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlanetManager : MonoBehaviour
 {
+  [SerializeField  ]
+  private UDateTime date;
+  public UDateTime Date
+  { get => date;
+  set{
+    date=value;
+    TimeChanged(value);
+  }
+
+  }
+  public event Action<DateTime> OnTimeChange;
   public static PlanetManager current;
+  private SolarSystemController solarSystemController;
+  public void TimeChanged(DateTime newTime)
+    {OnTimeChange?.Invoke(newTime);
+    }
+  void Update()
+    {
+       
+     
+}
 private void Awake()
 {
 if (current == null)
@@ -16,4 +37,5 @@ else
 Destroy(obj: this);
 }
 }
+
 }
--- Assets/MyScripts/SolarSystemController.cs
+++ Assets/MyScripts/SolarSystemController.cs
@@ -1,18 +1,36 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Data;
 using UnityEngine;
 
 public class SolarSystemController : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
-    {
+    {         PlanetManager.current.Date= new DateTime(2002, 01, 20);
+        PlanetManager.current.OnTimeChange += UpdatePosition;
         
     }
 
     // Update is called once per frame
     void Update()
-    {
+    { PlanetManager.current.Date =  ((DateTime) PlanetManager.current.Date).AddDays(1);
+        Debug.Log("Update");
+      
         
+        
+    }
+    public void UpdatePosition(System.DateTime t){
+        //GameObject.Find("Earth").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Earth,t);
+        
+        GameObject.Find("Jupiter").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Jupiter,t);
+
+         GameObject.Find("Mars").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Mars,t);
+         GameObject.Find("Mercury").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Mercury,t);
+        GameObject.Find("Venus").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Venus,t);
+        GameObject.Find("Saturn").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Saturn,t);
+        GameObject.Find("Uranus").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Uranus,t);
+        GameObject.Find("Neptune").transform.position= PlanetData.GetPlanetPosition(PlanetData.Planet.Neptune,t);
     }
 }
