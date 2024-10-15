84da10262dcc10d400d240602cdfbc86e3557d46
m23loure <162428618+m23loure@users.noreply.github.com>
2024-10-15 11:20:56 +0200

    Initial commit

---
+++ Assets/MyScripts/PlanetManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetManager : MonoBehaviour
+{
+  public static PlanetManager current;
+private void Awake()
+{
+if (current == null)
+{
+current = this;
+}
+else
+{
+Destroy(obj: this);
+}
+}
+}
